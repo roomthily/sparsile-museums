0c33d0619b93196688a1c512cef9a69974635a72
roomthily <sorenscott@gmail.com>
2016-01-24 10:20:16 -0700

    re-org

--- sparsile/sentiment.py
+++ sparsile/sentiment.py
@@ -1,6 +1,7 @@
 from vaderSentiment import sentiment as vader_sentiment
 from collections import defaultdict
 import statistics
+from colormath.color_objects import sRGBColor
 
 
 '''
@@ -16,20 +17,50 @@ so you could ask the bot for one of those things
 '''
 
 
-def run_sentiment(inputs):
+class SentimentMapper(object):
     '''
-    returns mean sentiment scores (mean of each bucket)
-    inputs = an array of string inputs, ie sentences
-    or paragraphs from a source.
-
-    ex output:
-        {'neg': 0.0, 'neu': 0.508, 'pos': 0.492, 'compound': 0.4404}
     '''
-    dd = defaultdict(list)
-    for i in inputs:
-        vs = vader_sentiment(i)
-        for k, v in vs.iteritems():
-            dd[k].append(v)
+    def __init__(self, inputs, aggregate):
+        self.inputs = inputs
+        self.aggregate = aggregate
+        self.scores = defaultdict(list)
 
-    # map the average
-    return {k: statistics.mean(v) for k, v in dd.items()}
+    def analyze(self):
+        pass
+
+    def _execute(self):
+        for i in self.inputs:
+            vs = vader_sentiment(i)
+            for k, v in vs.iteritems():
+                self.scores[k].append(v)
+
+    def aggregate(self):
+        if self.aggregate == 'positive':
+            self.agg_scores = {}
+        elif self.aggregate == 'negative':
+            self.agg_scores = {}
+        elif self.aggregate == 'mean':
+            self.agg_scores = {
+                k: statistics.mean(v) for k, v in self.scores.items()
+            }
+        else:
+            self.agg_scores = {}
+
+    def colorize(self):
+        '''
+        convert the sentiment array [negative, neutral, positive, compound]
+        into a color value
+
+        the sentiment analysis returns a dict of 0-1 floats, we use
+        the first three for the RGB values but a) it doesn't map to
+        anything like colors associated with moods (that we would recognize)
+        and b) currently just in the order provided. so it generates
+        RGB and it generates the hex but it's goofy.
+        '''
+        srgb = sRGBColor(
+            self.agg_scores['neg'],
+            self.agg_scores['neu'],
+            self.agg_scores['pos'],
+            False
+        )
+        return srgb.get_rgb_hex()
--- tests/test_color.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from sparsile.color import SentimentColor
-
-
-class ColorTests(unittest.TestCase):
-    pass
